bc5470ed33fd8e903cd20d668c1f2c1d12b55106
JQueimado <jpqueimado@gmail.com>
2021-04-21 18:36:40 +0100

    Enigma.py implementation

---
+++ Enigma.py
@@ -0,0 +1,21 @@
+
+## Disks ##
+disk1 = []
+disk2 = []
+disk3 = []
+
+start_pos_1 = 0
+start_pos_2 = 0
+start_pos_3 = 0
+
+turn_point_1 = 0
+turn_point_2 = 0
+
+## Reflector ##
+reflector = []
+
+## Cable board ##
+cables_config = {}
+
+if __name__ == "__main__":
+    pass
